20b05d7e8c3e69a4c4c0a4d29de6ef933fb6e1d1
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-15 18:26:15 -0700

    add entry point, Main class

---
+++ src/code/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+/**
+ * Starts the chess game application
+ */
+public class Main {
+    /**
+     * Runs the application
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args)
+    {
+        System.out.println("test");
+    }
+}
