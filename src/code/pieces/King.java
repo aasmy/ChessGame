0aa1702eb605f6bf820348bda0e2704adc26fc3f
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-19 23:38:38 -0700

    add king piece

---
+++ src/code/pieces/King.java
@@ -0,0 +1,43 @@
+package pieces;
+
+import components.Player;
+import components.Square;
+
+/**
+ * Represents a king chess piece
+ */
+public class King extends Piece {
+    private static final String NAME = "King";
+    private static final String SYMBOL = "K";
+
+    /**
+     * Creates a king piece with an owner and a starting location
+     *
+     * @param owner the player who owns this piece
+     * @param location the starting location of this piece
+     */
+    public King(final Player owner,
+                final Square location) {
+        super(owner, location);
+    }
+
+    /**
+     * Returns the king piece name
+     *
+     * @return the piece name
+     */
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    /**
+     * Returns the king piece symbol
+     *
+     * @return the piece symbol
+     */
+    @Override
+    public String getSymbol() {
+        return SYMBOL;
+    }
+}
