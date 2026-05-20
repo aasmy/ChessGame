4ac17a80edb9d4a1dab61c77bffc10b5851b9c87
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-19 23:40:38 -0700

    add queen

---
+++ src/code/pieces/Queen.java
@@ -0,0 +1,43 @@
+package pieces;
+
+import components.Player;
+import components.Square;
+
+/**
+ * Represents a queen chess piece
+ */
+public class Queen extends Piece {
+    private static final String NAME = "Queen";
+    private static final String SYMBOL = "Q";
+
+    /**
+     * Creates a queen piece with an owner and a starting location
+     *
+     * @param owner the player who owns this piece
+     * @param location the starting location of this piece
+     */
+    public Queen(final Player owner,
+                 final Square location) {
+        super(owner, location);
+    }
+
+    /**
+     * Returns the queen piece name
+     *
+     * @return the piece name
+     */
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    /**
+     * Returns the queen piece symbol
+     *
+     * @return the piece symbol
+     */
+    @Override
+    public String getSymbol() {
+        return SYMBOL;
+    }
+}
