99c09f995d0eb7973aec24407f8c60027b6e55fe
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-19 23:44:09 -0700

    add bishop

---
+++ src/code/pieces/Bishop.java
@@ -0,0 +1,43 @@
+package pieces;
+
+import components.Player;
+import components.Square;
+
+/**
+ * Represents a bishop chess piece
+ */
+public class Bishop extends Piece {
+    private static final String NAME = "Bishop";
+    private static final String SYMBOL = "B";
+
+    /**
+     * Creates a bishop piece with an owner and a starting location
+     *
+     * @param owner the player who owns this piece
+     * @param location the starting location of this piece
+     */
+    public Bishop(final Player owner,
+                  final Square location) {
+        super(owner, location);
+    }
+
+    /**
+     * Returns the bishop piece name
+     *
+     * @return the piece name
+     */
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    /**
+     * Returns the bishop piece symbol
+     *
+     * @return the piece symbol
+     */
+    @Override
+    public String getSymbol() {
+        return SYMBOL;
+    }
+}
