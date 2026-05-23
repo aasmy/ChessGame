15819871a74bbbf2695e63a24d51bffe7b48e220
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-22 20:21:22 -0700

    add basic pawn forward move

---
+++ src/code/movements/OneSquareForwardMoveStrategy.java
@@ -0,0 +1,47 @@
+package movements;
+
+import board.Board;
+import components.Square;
+import handlers.EmptyDestinationHandler;
+import handlers.ExistingPieceAtStartHandler;
+import handlers.Handler;
+import handlers.OneSquareForwardHandler;
+import handlers.ValidBoardHandler;
+import handlers.ValidDestinationHandler;
+import handlers.ValidStartLocationHandler;
+
+/**
+ * Validates one square forward pawn moves
+ */
+public class OneSquareForwardMoveStrategy implements MoveStrategy {
+    /**
+     * Checks whether a pawn can move one square forward
+     *
+     * @param board the board where the move is checked
+     * @param from the starting square
+     * @param to the destination square
+     * @return true if the pawn can move one square forward
+     */
+    @Override
+    public boolean validateMove(final Board board,
+                                final Square from,
+                                final Square to)
+    {
+        final Handler validationChain = buildValidationChain();
+
+        return validationChain.canHandle(board, from, to);
+    }
+
+    private Handler buildValidationChain() {
+        final Handler validBoard = new ValidBoardHandler();
+
+        validBoard
+                .setNext(new ValidStartLocationHandler())
+                .setNext(new ValidDestinationHandler())
+                .setNext(new ExistingPieceAtStartHandler())
+                .setNext(new EmptyDestinationHandler())
+                .setNext(new OneSquareForwardHandler());
+
+        return validBoard;
+    }
+}
