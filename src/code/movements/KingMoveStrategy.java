f74eb5cc2b8ddbdb05983bb97bd714c8cf9cdbf8
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-22 20:19:40 -0700

    add king move strategy

---
+++ src/code/movements/KingMoveStrategy.java
@@ -0,0 +1,44 @@
+package movements;
+
+import board.Board;
+import components.Square;
+import handlers.ExistingPieceAtStartHandler;
+import handlers.Handler;
+import handlers.KingMoveHandler;
+import handlers.NotSameColorAtDestinationHandler;
+import handlers.ValidBoardHandler;
+import handlers.ValidDestinationHandler;
+import handlers.ValidStartLocationHandler;
+
+/**
+ * Validates king chess moves
+ */
+public class KingMoveStrategy implements MoveStrategy {
+    /**
+     * Checks whether a move is valid for a king
+     *
+     * @param board the board where the move is checked
+     * @param from the starting square
+     * @param to the destination square
+     * @return true if the move is valid for a king
+     */
+    @Override
+    public boolean validateMove(final Board board, final Square from, final Square to) {
+        final Handler validationChain = buildValidationChain();
+
+        return validationChain.canHandle(board, from, to);
+    }
+
+    private Handler buildValidationChain() {
+        final Handler validBoard = new ValidBoardHandler();
+
+        validBoard
+                .setNext(new ValidStartLocationHandler())
+                .setNext(new ValidDestinationHandler())
+                .setNext(new ExistingPieceAtStartHandler())
+                .setNext(new NotSameColorAtDestinationHandler())
+                .setNext(new KingMoveHandler());
+
+        return validBoard;
+    }
+}
