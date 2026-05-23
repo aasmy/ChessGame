218d4dac87724c8cd6e7b7e8618aa7c8c3edbc26
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-22 18:25:29 -0700

    create diagonal move strategy

---
+++ src/code/movements/DiagonalMoveStrategy.java
@@ -0,0 +1,49 @@
+package movements;
+
+import board.Board;
+import components.Square;
+import handlers.DiagonalMoveHandler;
+import handlers.ExistingPieceAtStartHandler;
+import handlers.Handler;
+import handlers.NoPiecesBetweenHandler;
+import handlers.NotSameColorAtDestinationHandler;
+import handlers.ValidBoardHandler;
+import handlers.ValidDestinationHandler;
+import handlers.ValidStartLocationHandler;
+
+/**
+ * Validates diagonal chess moves
+ */
+public class DiagonalMoveStrategy implements MoveStrategy {
+    /**
+     * Checks whether a move is valid diagonally
+     *
+     * @param board the board where the move is checked
+     * @param from the starting square
+     * @param to the destination square
+     * @return true if the move is valid diagonally
+     */
+    @Override
+    public boolean validateMove(final Board board,
+                                final Square from,
+                                final Square to)
+    {
+        final Handler validationChain = buildValidationChain();
+
+        return validationChain.canHandle(board, from, to);
+    }
+
+    private Handler buildValidationChain() {
+        final Handler validBoard = new ValidBoardHandler();
+
+        validBoard
+                .setNext(new ValidStartLocationHandler())
+                .setNext(new ValidDestinationHandler())
+                .setNext(new ExistingPieceAtStartHandler())
+                .setNext(new NotSameColorAtDestinationHandler())
+                .setNext(new DiagonalMoveHandler())
+                .setNext(new NoPiecesBetweenHandler());
+
+        return validBoard;
+    }
+}
