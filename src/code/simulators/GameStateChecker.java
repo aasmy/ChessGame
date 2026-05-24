2292be5cb351a3d4d8cf6ac7127d324056567198
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-24 16:27:36 -0700

    create checkmate detection

--- src/code/simulators/GameStateChecker.java
+++ src/code/simulators/GameStateChecker.java
@@ -11,6 +11,7 @@ import java.util.List;
  * Checks simple chess game states
  */
 public final class GameStateChecker {
+
     private GameStateChecker() {
     }
 
@@ -36,6 +37,82 @@ public final class GameStateChecker {
         }
 
         return false;
+    }
+
+    /**
+     * Checks whether the king with the given color is in checkmate
+     *
+     * @param board the chess board
+     * @param kingColor the king color to check
+     * @return true if the king is in checkmate
+     */
+    public static boolean isCheckmate(final Board board, final Color kingColor) {
+        validateInputs(board, kingColor);
+
+        if (!isCheck(board, kingColor)) {
+            return false;
+        }
+
+        final List<Piece> playerPieces = board.getPiecesByColor(kingColor);
+
+        for (final Piece piece : playerPieces) {
+            if (hasAnyLegalMove(board, piece)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the piece has at least one legal move
+     *
+     * @param board the chess board
+     * @param piece the piece to test
+     * @return true if the piece has a legal move
+     */
+    private static boolean hasAnyLegalMove(final Board board, final Piece piece) {
+        for (int row = 0; row < board.getRows(); row++) {
+            for (int column = 0; column < board.getColumns(); column++) {
+                final Square target = new Square(row, column);
+
+                if (piece.canMove(board, target)
+                        && !leavesKingInCheck(board, piece, target)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks whether moving the piece would leave its king in check
+     *
+     * @param board the chess board
+     * @param piece the piece to move
+     * @param target the target square
+     * @return true if the move leaves the king in check
+     */
+    private static boolean leavesKingInCheck(final Board board,
+                                             final Piece piece,
+                                             final Square target) {
+        final Square originalLocation = piece.getLocation();
+        final Piece capturedPiece = board.getPieceAt(target);
+
+        board.movePiece(originalLocation, target);
+
+        final boolean kingInCheck = isCheck(board, piece.getOwner().getColor());
+
+        board.setPieceAt(piece, originalLocation);
+
+        if (capturedPiece == null) {
+            board.removePieceAt(target);
+        } else {
+            board.setPieceAt(capturedPiece, target);
+        }
+
+        return kingInCheck;
     }
 
     /**
